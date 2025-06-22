1f8d5f156743ce76bcd11763cf5431092e4eb8eb
André devkcud Albanese Junior <patommmmm@proton.me>
2025-06-22 03:55:06 -0300

    refactor(tree): Cleanup and removal of unused logical blocks
    For example the `AddDirectory` inside `AddFile`

--- pkg/tree/tree.go
+++ pkg/tree/tree.go
@@ -25,16 +25,13 @@ const (
 )
 
 func New(baseDir string) (root *Node, current *Node) {
-	if baseDir == "~" || strings.HasPrefix(baseDir, "~/") {
-		home, err := os.UserHomeDir()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "warning: could not get home dir: %v\n", err)
+	if !filepath.IsAbs(baseDir) || baseDir == "~" {
+		home, _ := os.UserHomeDir()
+
+		if baseDir == "~" {
+			baseDir = home
 		} else {
-			if baseDir == "~" {
-				baseDir = home
-			} else {
-				baseDir = utils.JoinPaths(home, baseDir[2:])
-			}
+			baseDir = utils.JoinPaths(home, baseDir)
 		}
 	}
 
@@ -66,32 +63,12 @@ func (n *Node) Up() *Node {
 	return n.parent
 }
 
-func findStart(root *Node, parts []string) (current *Node, start int) {
-	for root.parent != nil {
-		root = root.parent
-	}
-	current = root
-
-	start = 0
-	if len(parts) > 0 && parts[0] == root.relativePath {
-		start = 1
-	}
-
-	return
-}
-
 func (n *Node) AddDirectory(path string) *Node {
 	if n.children == nil {
 		return n
 	}
 
-	if filepath.IsAbs(path) || strings.HasPrefix(path, "~/") {
-		if strings.HasPrefix(path, "~/") {
-			home, err := os.UserHomeDir()
-			if err == nil {
-				path = filepath.Join(home, path[2:])
-			}
-		}
+	if filepath.IsAbs(path) {
 		path = utils.CleanPath(path)
 		parts := utils.SplitPath(path)
 
@@ -126,42 +103,40 @@ func (n *Node) AddFile(path string) *Node {
 		return n
 	}
 
-	if filepath.IsAbs(path) || strings.HasPrefix(path, "~/") {
-		if strings.HasPrefix(path, "~/") {
-			home, err := os.UserHomeDir()
-			if err == nil {
-				path = utils.JoinPaths(home, path[2:])
-			}
-		}
-		path = utils.CleanPath(path)
-		parts := utils.SplitPath(path)
-
-		current, start := findStart(n, parts)
-
-		for _, part := range parts[start : len(parts)-1] {
-			if part == "" {
-				continue
-			}
-			current = current.AddDirectory(part)
-		}
-
-		fileName := parts[len(parts)-1]
-		node := &Node{
-			relativePath: fileName,
-			parent:       current,
-			children:     nil,
-		}
-		current.children = append(current.children, node)
-		return node
-	}
-
 	node := &Node{
 		relativePath: path,
 		parent:       n,
 		children:     nil,
 	}
+
 	n.children = append(n.children, node)
+
 	return node
+}
+
+func (n *Node) FullPath() string {
+	if n.parent == nil {
+		return n.relativePath
+	}
+	return filepath.Join(n.parent.FullPath(), n.relativePath)
+}
+
+func (n *Node) GetChildren() []*Node {
+	return n.children
+}
+
+func findStart(root *Node, parts []string) (current *Node, start int) {
+	for root.parent != nil {
+		root = root.parent
+	}
+	current = root
+
+	start = 0
+	if len(parts) > 0 && parts[0] == root.relativePath {
+		start = 1
+	}
+
+	return
 }
 
 func (n *Node) PrintTree() {
@@ -202,55 +177,6 @@ func (n *Node) print(prefix string, isTail bool, depth int) {
 			newPref = prefix + "│   "
 		}
 		child.print(newPref, i == len(node.children)-1, depth+1)
-	}
-}
-
-func (n *Node) FullPath() string {
-	if n.parent == nil {
-		return n.relativePath
-	}
-	return filepath.Join(n.parent.FullPath(), n.relativePath)
-}
-
-func (n *Node) GetChildren() []*Node {
-	return n.children
-}
-
-func (n *Node) PrintFiles() {
-	n.walkFiles(func(file *Node) {
-		p := file.FullPath()
-
-		dir := filepath.Dir(p)
-		for {
-			info, err := os.Stat(dir)
-			if err != nil {
-				if os.IsNotExist(err) {
-					dir = filepath.Dir(dir)
-					continue
-				}
-			} else if !info.IsDir() {
-				dir = filepath.Dir(dir)
-				continue
-			}
-			break
-		}
-
-		needsElev := utils.NeedsElevation(dir)
-		if needsElev {
-			fmt.Printf("%s (sudo required)\n", p)
-		} else {
-			fmt.Println(p)
-		}
-	})
-}
-
-func (n *Node) walkFiles(fn func(*Node)) {
-	if n.children == nil {
-		fn(n)
-		return
-	}
-	for _, c := range n.children {
-		c.walkFiles(fn)
 	}
 }
 
